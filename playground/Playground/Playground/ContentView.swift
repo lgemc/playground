fd4afc7ea02f3ad8b9992f7a125c33c8e5344643
Luis Gerardo Manrique Cardona <luis.manrique.car@gmail.com>
2026-03-02 11:57:51 -0500

    feat: add native ios implementation for ondevice executions

---
+++ playground/Playground/Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Luis Gerardo Manrique Cardona on 2/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worlds!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ playground/Playground/Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Luis Gerardo Manrique Cardona on 2/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
